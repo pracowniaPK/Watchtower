6c9845ac24fa7ce0927ad26ca567a89a516efda1
PKacz <kaczory5@gmail.com>
2019-06-09 17:06:38 +0200

    switch to apscheduler
    + add app factory

--- watchtower/app.py
+++ watchtower/app.py
@@ -4,59 +4,72 @@ import os
 import math
 
 from flask import Flask, current_app, render_template
-from flask_apscheduler import APScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
 from db import get_db, init_db
-from config import Config
+from config import Config, sites_list
 from model import check
 
 
-app = Flask(__name__)
-
-def check_job(sites_list):
+def check_job(sites_list, record=True):
     res = {}
     for address, pattern in sites_list:
         t1 = time.time()
         try:
-            r = requests.get(address)
-        except requests.ConnectionError as e:
+            r = requests.get(address, timeout=5)
+        except (requests.ConnectionError, requests.exceptions.ReadTimeout) as e:
             res[address] = check(address, 0, 0, notes=str(e))     
             continue
         t2 = time.time()
         dt = math.floor((t2-t1)*1000)
+
         res[address] = check(address, r.status_code, dt)
         if pattern not in r.text:
             res[address].ok = False
             res[address].notes = 'Pattern does not match.'
-    
-    print(datetime.datetime.now())
-    for address in res:
+
+    if record:    
+        for address in res:
+            with app.app_context():
+                res[address].db_insert()
+
+    print('check done at {}'.format(datetime.datetime.now()))
+    return res
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config())
+    app.config.from_mapping(DATABASE=os.path.join(app.instance_path, 'db.sqlite'))
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
         with app.app_context():
-            res[address].db_insert()
-    print()
+            latest = check.db_get_latest()
+        return render_template('index.html', checks=latest)
 
-app.config.from_object(Config())
-app.config.from_mapping(DATABASE=os.path.join(app.instance_path, 'db.sqlite'))
+    @app.route('/timeout')
+    def timeout():
+        time.sleep(6)
+        return 'am sorry i\'m late'
 
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_job, 'interval', [sites_list], seconds=120) # minutes=120)
+    scheduler.start()
 
-@app.route('/')
-def index():
-    with app.app_context():
-        latest = check.db_get_latest()
-    return render_template('index.html', checks=latest)
+    return app
+    
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
 #     with app.app_context():
 #         init_db()
 
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
-
-app.run(use_reloader=False)
+    app = create_app()
+    app.run() # use_reloader=False)
--- watchtower/config.py
+++ watchtower/config.py
@@ -1,22 +1,13 @@
 sites_list = [
     ('https://www.google.com/', 'search'),
     ('https://nope.oi/', 'stuff'),
+    ('https://bioinfo.pl/', 'BioInfoBank Institute'),
     ('https://www.yahoo.com/', 'qwerasdf'),
     ('http://www.error418.net/', 'teapot'),
+    ('http://127.0.0.1:5000/timeout', 'late'),
 ]
 
 class Config:
     DEVELOPMENT = True
     DEBUG = True
     SECRET_KEY='dev'
-
-    JOBS = [
-        {
-            'id': 'check_job',
-            'func': 'app:check_job',
-            'args': [sites_list],
-            'trigger': 'interval',
-            'seconds': 60
-        }
-    ]
-    SCHEDULER_API_ENABLED = True
